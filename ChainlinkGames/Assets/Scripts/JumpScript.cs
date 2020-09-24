ba6103c3fcff796ab76e559bb68db85296403824
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-09-24 11:29:07 +0100

    :sparkles: healthbar and basic movement added :sparkles:

---
+++ ChainlinkGames/Assets/Scripts/JumpScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    public float jumpHeight;
+    private bool isJumping = false;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(Vector2.up * jumpHeight);
+            isJumping = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+        }
+    }
+}
--- ChainlinkGames/Assets/Scripts/PauseGame.cs
+++ ChainlinkGames/Assets/Scripts/PauseGame.cs
@@ -11,7 +11,7 @@ public class PauseGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.Space))
+       if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused == true)
             {
